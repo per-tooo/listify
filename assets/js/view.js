6066bfb32800043a8d2c4c2c88ad60bfa6fc88ac
Simon Ruhland <ruhland@uni-potsdam.de>
2025-04-19 18:51:46 +0200

    + add/rem items logic

--- assets/js/view.js
+++ assets/js/view.js
@@ -1,3 +1,30 @@
 let items = document.querySelector(".items");
+let item = document.querySelector("template");
+let input = document.querySelector("input");
 
-let Sortable = new Sortable.create(items);
+function newIdentifier() {
+  return items.childElementCount;
+}
+
+function addItemToList(event) {
+  if (event.keyCode === 13) {
+    let clone = item.content.cloneNode(true);
+    let actions = clone.querySelector(".actions");
+    clone.querySelector(".text span").textContent = input.value;
+
+    clone.firstElementChild.setAttribute("item", newIdentifier());
+    actions.setAttribute("target", newIdentifier());
+
+    actions.addEventListener("click", () => {
+      items.removeChild(
+        document.querySelector(`[item="${actions.getAttribute("target")}"]`)
+      );
+    });
+
+    items.appendChild(clone);
+  }
+}
+
+input.addEventListener("keypress", (event) => {
+  addItemToList(event);
+});
